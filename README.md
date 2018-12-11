b51f8b500546723882b0ec19ea790d98d1c14362
Kevin Jullien <hi@juke.fr>
2018-12-11 12:42:09 +0100

    nobody likes oop

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # googlemaps-2-mapbox
+
 Convert Google Maps IDs to Mapbox IDs
+Checks if the geometric coordinates (lat/lng) are within tolerated bounds (default is 0.5% tolerance).
 
 ## Usage
 
@@ -8,11 +10,13 @@ $ npm i -S googlemaps-2-mapbox
 ```
 
 ```javascript
-const Geoconverter = require('googlemaps-2-mapbox')
-const converter = new Geocoverter({
-    google: process.env.GOOGLE_GEOCODING_API_KEY,
-    mapbox: process.env.MAPBOX_API_KEY
-})
+const convert = require("googlemaps-2-mapbox");
 
-const id = converter.convert('XXXXXXXXX') // returns a Promise
+const options = {
+  google: process.env.GOOGLE_GEOCODING_API_KEY,
+  mapbox: process.env.MAPBOX_API_KEY,
+  tolerance: 3 // optionnal, default is 0.5%
+};
+
+const mapboxID = await convert(googleID, options);
 ```
--- index.js
+++ index.js
@@ -1,38 +1,86 @@
 /**
- * Converts a Google Place ID to a Mapbox ID
- * 
- * const converter = new Geoconverter({google: 'xxx', mapbox: 'xxx'})
- * const newId = await converter.convert('XXXXXXXX')
+ * Converts a Google Place ID to a Mapbox ID and check if geometry matches (default 0.5% tolerance)
+ *
+ * const id = convert('google-maps-id', {google: 'apikey', mapbox: 'apikey', tolerance: 1} )
  */
-module.exports = class Geoconverter {
-    constructor(options) {
-        this.options = options
-        this.init()
-    }
+module.exports = (
+  id = "",
+  { google = "", mapbox = "", tolerance = 0.5 } = {}
+) => {
+  /**
+   * Creates API clients
+   */
+  const init = () => {
+    this.googleClient = require("@google/maps").createClient({
+      key: google,
+      Promise: Promise
+    });
+    this.mapboxClient = require("@mapbox/mapbox-sdk/services/geocoding")({
+      accessToken: mapbox
+    });
+  };
 
-    init() {
-        this.googleClient = require('@google/maps').createClient({
-            key: this.options.google,
-            Promise: Promise
-          });
-        this.mapboxClient = require('@mapbox/mapbox-sdk/services/geocoding')({ accessToken: this.options.mapbox })
-    }
+  /**
+   * Google ID to Address
+   * @param {*} id
+   */
+  const googleAddress = id => {
+    return this.googleClient.reverseGeocode({ place_id: id }).asPromise();
+  };
 
-    googleAddress(id) {
-        return this.googleClient.reverseGeocode({place_id: id}).asPromise()
-    }
+  /**
+   * Address to Mapbox ID
+   * Check if geometric position is within tolerance (default is 0.5%)
+   * @param {*} address
+   */
+  const mapboxId = ({ formatted_address, geometry }) => {
+    return this.mapboxClient
+      .forwardGeocode({
+        query: formatted_address,
+        limit: 1
+      })
+      .send()
+      .then(response => {
+        const latRatio =
+          response.body.features[0].geometry.coordinates[1] /
+          geometry.location.lat;
+        const lngRatio =
+          response.body.features[0].geometry.coordinates[0] /
+          geometry.location.lng;
 
-    mapboxId(address) {
-        return this.mapboxClient.forwardGeocode({
-            query: address,
-            limit: 1
-          })
-          .send()
-    }
+        const lowerBound = 1 - tolerance / 100;
+        const upperBound = 1 + tolerance / 100;
 
-    convert(id) {
-        return this.googleAddress(id)
-            .then(address => this.mapboxId(address.json.results[0].formatted_address))
-            .then(id => id.body.features[0].id)
-    }
-}
+        if (
+          lowerBound <= latRatio &&
+          latRatio <= upperBound &&
+          lowerBound <= lngRatio &&
+          lngRatio <= upperBound
+        ) {
+          return response.body.features[0].id; // mapbox place id
+        } else {
+          throw new Error(
+            `The Google Maps and Mapbox position (lat/lng) did not match within ${tolerance}%.
+            Lat : ${lowerBound} <= ${latRatio} <= ${upperBound}
+            Lng : ${lowerBound} <= ${lngRatio} <= ${upperBound}`
+          );
+        }
+      });
+  };
+
+  const convert = id => {
+    return googleAddress(id).then(address => mapboxId(address.json.results[0]));
+  };
+
+  google && mapbox
+    ? init()
+    : (() => {
+        throw new Error("Missing API key(s).");
+      })();
+
+  return id
+    ? convert(id)
+    : (() => {
+        throw new Error("Missing ID key.");
+      })();
+};
