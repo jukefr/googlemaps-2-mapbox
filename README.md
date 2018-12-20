1bf2d50c88c772d7c8ed58f632cefda723117936
Kevin Jullien <hi@juke.fr>
2018-12-20 12:43:15 +0100

    typescript rework and used my brain to convert this time (1000% more accurate)

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # googlemaps-2-mapbox
 
-Convert Google Maps IDs to Mapbox IDs.
-
-Checks if the geometric coordinates (lat/lng) are within tolerated bounds (default is 0.5% tolerance).
+Convert Google Maps IDs to Mapbox IDs :
+**GOOGLE ID => LAT/LNG => MAPBOX ID** (Most precise way to do it)
 
 ## Usage
 
@@ -13,11 +12,15 @@ $ npm i -S googlemaps-2-mapbox
 ```javascript
 const convert = require("googlemaps-2-mapbox");
 
+const googleID = 'XXXXX'
+
 const options = {
   google: process.env.GOOGLE_GEOCODING_API_KEY,
   mapbox: process.env.MAPBOX_API_KEY,
-  tolerance: 3 // optionnal, default is 0.5%
 };
 
 const mapboxID = await convert(googleID, options);
 ```
+
+## Information
+This package is written in typescript but also pushed to npm as a built javascript. It is compatible both with Typescript and Javascript.
---
+++ index.ts
@@ -0,0 +1,77 @@
+import { createClient as googleClient } from "@google/maps";
+const mapboxClient = require("@mapbox/mapbox-sdk/services/geocoding");
+
+const checkParameters = ({
+  id,
+  google,
+  mapbox
+}: {
+  id: string;
+  google: string;
+  mapbox: string;
+}) => {
+  id ||
+    (() => {
+      throw new Error("Missing Google Spot ID.");
+    })();
+  (google && mapbox) ||
+    (() => {
+      throw new Error("Missing API key(s).");
+    })();
+};
+
+const createClients = ({
+  google,
+  mapbox
+}: {
+  google: string;
+  mapbox: string;
+}) => ({
+  googleClient: googleClient({
+    key: google,
+    Promise: Promise
+  }),
+  mapboxClient: mapboxClient({
+    accessToken: mapbox
+  })
+});
+
+const googleSpotToLocation = async ({
+  id,
+  clients
+}: {
+  id: string;
+  clients: any;
+}) =>
+  (await clients.googleClient.reverseGeocode({ place_id: id }).asPromise()).json
+    .results[0].geometry.location;
+
+const mapboxLocationToAddress = async ({
+  lat,
+  lng,
+  clients
+}: {
+  lat: string;
+  lng: string;
+  clients: any;
+}) =>
+  (await clients.mapboxClient
+    .reverseGeocode({
+      query: [lng, lat],
+      mode: "mapbox.places",
+      limit: 1
+    })
+    .send()).body.features[0].id;
+
+export = async (
+  id = "",
+  { google = "", mapbox = "" } = {}
+): Promise<string> => {
+  checkParameters({ id, mapbox, google });
+
+  const clients = createClients({ google, mapbox });
+
+  const { lat, lng } = await googleSpotToLocation({ id, clients });
+
+  return mapboxLocationToAddress({ lat, lng, clients });
+};
